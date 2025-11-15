99c6aa7c3d71af1a7ef1df1e88fe19d5fdbb3c38
Kshitij Kaushik <97613721+KshitijKaushik123@users.noreply.github.com>
2025-11-15 13:52:22 +0530

    testing- git

--- src/main/java/com/stunner/auth/controller/AuthController.java
+++ src/main/java/com/stunner/auth/controller/AuthController.java
@@ -4,15 +4,14 @@ import com.stunner.auth.dto.LoginOtpRequestDto;
 import com.stunner.auth.dto.SignupOtpRequestDto;
 import com.stunner.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@RequiredArgsConstructor
 public class AuthController {
-    private final AuthService authService;
-
-
+    @Autowired
+    AuthService authService;
     @PostMapping("/signup/request-otp")
     public String requestSignupOtp(@RequestBody SignupOtpRequestDto dto) {
         return authService.requestSignupOtp(dto);
